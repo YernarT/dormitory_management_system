9322d61e2fa2bef30d3c9d626305be5f9b471788
YernarT <toktaryernar@gmail.com>
2022-03-15 21:11:39 +0600

    feat: :sparkles: 套用 react_template 代码库

---
+++ frontend/src/service/ajax.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { localStorage } from '@/utils';
+
+export const apiServerInstance = axios.create({
+	baseURL: 'http://localhost:8000',
+	validateStatus: status => status >= 200 && status < 300,
+});
+
+// Request interceptor
+apiServerInstance.interceptors.request.use(config => {
+	// Every time the jwt is updated in the page, the localstorage needs to be updated together
+	const { jwt } = localStorage.get('user');
+
+	if (jwt && config.headers) {
+		config.headers['Authorization'] = `Bearer ${jwt}`;
+	}
+
+	return config;
+});
+
+// Response interceptor
+apiServerInstance.interceptors.response.use(
+	response => response.data,
+	error => {
+		if (error.response && error.response.status) {
+			let { status } = error.response;
+
+			if (status >= 500) {
+				return Promise.reject({
+					message: 'The server crashed...',
+				});
+			}
+
+			if (status >= 400) {
+				return Promise.reject(error.response.data);
+			}
+		}
+
+		if (error.message === 'Network Error') {
+			return Promise.reject({
+				message: 'Server failed, try again later',
+			});
+		}
+
+		console.error('Error in request: ', error);
+		return Promise.reject(error);
+	},
+);
---
+++ frontend/src/service/api/auth-api.js
@@ -0,0 +1,8 @@
+import { apiServerInstance } from '../ajax';
+
+// Login
+export const reqLogin = data => apiServerInstance.post('/user/login', data);
+
+// Register
+export const reqRegister = data =>
+	apiServerInstance.post('/user/register', data);
