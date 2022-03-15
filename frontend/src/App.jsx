cfdeedfcf09dba92e7b3df0d1f3dd483b461c2e2
YernarT <toktaryernar@gmail.com>
2022-03-15 21:11:07 +0600

    feat: :sparkles: 套用 react_template 代码库

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,41 @@
 import React from 'react';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-// import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 
-// import { RouteGuard } from '@/components';
-// import { CssBaseLine, ToastContainer } from '@/compact-ui';
+import { RouteGuard, CssBaseLine } from '@/components';
 
-// import { useEventListener, useCreation } from 'ahooks';
-// import { localStorage } from '@/utils';
+import { useEventListener, useCreation } from 'ahooks';
+import { localStorage } from '@/utils';
 
-// import { useRecoilValue } from 'recoil';
-// import { userAtom, pageAtom } from '@/store';
+import { useRecoilValue } from 'recoil';
+import { userAtom, pageAtom } from '@/store';
 
-// import getTheme from '@/assets/theme';
-// import routes from '@/routes';
+import getTheme from '@/assets/theme';
+import routes from '@/routes';
 
 export default function App() {
-	// const user = useRecoilValue(userAtom);
-	// const page = useRecoilValue(pageAtom);
+	const user = useRecoilValue(userAtom);
+	const page = useRecoilValue(pageAtom);
 
-	// // Refresh the page to save the data in Recoil to LocalStorage
-	// useEventListener('beforeunload', () => {
-	// 	localStorage.set('user', user);
-	// 	localStorage.set('page', page);
-	// });
+	// Refresh the page to save the data in Recoil to LocalStorage
+	useEventListener('beforeunload', () => {
+		localStorage.set('user', user);
+		localStorage.set('page', page);
+	});
 
 	// Theme object
-	// const theme = useCreation(() => getTheme(page.viewMode), [page.viewMode]);
+	const theme = useCreation(() => getTheme(), []);
 
 	return (
-        <div className="App">App</div>
+		// styled 主题
+		<StyledThemeProvider theme={theme}>
+			<BrowserRouter>
+				{/* 全局样式 */}
+				<CssBaseLine />
+				{/* 路由守卫 */}
+				<RouteGuard routes={routes} />
+			</BrowserRouter>
+		</StyledThemeProvider>
 	);
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,50 @@
 import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
-
 import { RecoilRoot } from 'recoil';
 
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import intervalPlural from 'i18next-intervalplural-postprocessor';
+import { kkKZ, enUS } from '@/i18n';
+
+import { getLatestState, localStorage, getHtmlLang } from '@/utils';
+import { defaultPageState } from '@/store';
+
+import { SafeArea } from './components/common';
 import App from './App';
+
+import 'antd/dist/antd.less';
+
+// Initialize language, get it from LocalStorage
+const [, page] = getLatestState(localStorage.get('page', {}), defaultPageState);
+
+i18next
+	.use(initReactI18next)
+	.use(intervalPlural)
+	.init({
+		resources: {
+			kkKZ: {
+				translation: kkKZ,
+			},
+			enUS: {
+				translation: enUS,
+			},
+		},
+		lng: page.locale,
+		fallbackLng: 'kkKZ',
+		compatibilityJSON: 'v3',
+	});
 
 render(
 	<StrictMode>
 		<RecoilRoot>
-			<App />
+			<SafeArea>
+				<App />
+			</SafeArea>
 		</RecoilRoot>
 	</StrictMode>,
 	document.getElementById('root'),
+	() => {
+		document.documentElement.lang = getHtmlLang(page.locale);
+	},
 );
