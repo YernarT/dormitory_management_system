96a8c80ddee631254f5bbd73a070a9153a5fc81f
YernarT <toktaryernar@gmail.com>
2022-03-17 21:55:09 +0600

    feat: :sparkles: add secondary style

--- frontend/src/assets/theme/light/typography.js
+++ frontend/src/assets/theme/light/typography.js
@@ -5,24 +5,25 @@ export default {
 		color: '#283252',
 
 		whiteSpace: 'nowrap',
+		marginBottom: 0,
 	},
 	text: {
 		fontWeight: 400,
 		fontSize: '0.9rem',
-		color: '#a2a5b9',
+		color: '#19191c',
+
+		marginBottom: 0,
+	},
+	secondary: {
+		fontWeight: 400,
+		fontSize: '0.9rem',
+		color: '#888b9b',
+
+		marginBottom: 0,
 	},
 	link: {
 		fontWeight: 400,
 		fontSize: '0.9rem',
 		color: '3d3df7',
-	},
-
-	// MENU ( Drawer & BottomNavigationBar ) Background Color
-	MENU_TEXT: {
-		fontWeight: 400,
-		fontSize: '1.15rem',
-		color: '#283252',
-
-		whiteSpace: 'nowrap',
 	},
 };
