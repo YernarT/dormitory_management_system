3c236ca333e2f6cf722a63950a1a0b5573b0124f
YernarT <toktaryernar@gmail.com>
2022-03-17 15:34:03 +0600

    fix: :bug: 删除临时代码

--- frontend/src/layout/CommonLayout/Content/index.jsx
+++
@@ -1,7 +0,0 @@
-import React, { memo } from 'react';
-
-import { ContentStyledBox } from './style';
-
-export default memo(function Content({ children }) {
-	return <ContentStyledBox>{children}</ContentStyledBox>;
-});
