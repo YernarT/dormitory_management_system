665e73937a18c3293088f35fe02f1bec96215202
YernarT <toktaryernar@gmail.com>
2022-03-17 21:55:55 +0600

    wip: :construction: Landing Page

--- frontend/src/pages/common/LandingPage/index.jsx
+++ frontend/src/pages/common/LandingPage/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { Result } from 'antd';
 import { CommonLayout } from '@/layout';
 import { LandingPageStyledBox } from './style';
 
 export default function LandingPage() {
 	return (
 		<CommonLayout>
-			<LandingPageStyledBox></LandingPageStyledBox>
+			<LandingPageStyledBox>
+				<Result status="success" />
+			</LandingPageStyledBox>
 		</CommonLayout>
 	);
 }
