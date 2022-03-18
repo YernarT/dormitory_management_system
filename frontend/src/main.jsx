0cea96aec0368bc3d57bc2fe9003d371723f42ad
YernarT <toktaryernar@gmail.com>
2022-03-18 13:50:30 +0600

    feat: :sparkles: 新增 antd 重置样式文件

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,6 +14,7 @@ import { SafeArea } from './components/common';
 import App from './App';
 
 import 'antd/dist/antd.less';
+import '@/assets/style/antd-mod.less';
 
 // Initialize language, get it from LocalStorage
 const [, page] = getLatestState(localStorage.get('page', {}), defaultPageState);
