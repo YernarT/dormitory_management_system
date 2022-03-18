568cc1a11ba0f3ff701a91a857c27aea09ba2001
YernarT <toktaryernar@gmail.com>
2022-03-18 17:22:34 +0600

    feat: :sparkles: landing page done

--- frontend/src/pages/common/LandingPage/index.jsx
+++ frontend/src/pages/common/LandingPage/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-import { Result } from 'antd';
 import { CommonLayout } from '@/layout';
 import { LandingPageStyledBox } from './style';
 
+import { welcomeBG } from '@/assets/image';
+
 export default function LandingPage() {
+	const { t } = useTranslation();
 	return (
 		<CommonLayout>
 			<LandingPageStyledBox>
-				<Result status="success" />
+				<div className="welcome">
+					<img src={welcomeBG} alt="background image" />
+					<div className="info">
+						<h1 className="title">{t('footer_site_name')}</h1>
+						<h3 className="sub-title">{t('landingP_welcome_subTitle')}</h3>
+					</div>
+				</div>
 			</LandingPageStyledBox>
 		</CommonLayout>
 	);
--- frontend/src/pages/common/LandingPage/style.js
+++ frontend/src/pages/common/LandingPage/style.js
@@ -1,3 +1,59 @@
 import styled from 'styled-components';
 
-export const LandingPageStyledBox = styled.div``;
+export const LandingPageStyledBox = styled.div`
+	height: inherit;
+
+	overflow: hidden auto;
+
+	.welcome {
+		position: relative;
+
+		height: 100%;
+
+		img {
+			position: absolute;
+			top: 0;
+			left: 0;
+			z-index: 1;
+
+			width: 100%;
+			height: 98%;
+
+			object-fit: cover;
+			filter: blur(4px);
+		}
+
+		.info {
+			position: absolute;
+			top: 40%;
+			z-index: 5;
+			transform: translateY(-40%);
+
+			width: 100%;
+			padding: 0 10px;
+
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			gap: 24px;
+
+			[class*='title'] {
+				line-height: 100%;
+				margin: 0;
+
+				color: #fff;
+				text-align: center;
+				text-shadow: 0 0 4px #000;
+			}
+
+			.title {
+				font-size: 2.4rem;
+			}
+
+			.sub-title {
+				font-size: 1.4rem;
+			}
+		}
+	}
+`;
