65ffeccdfd171832b3e357e9f99f7f32448342a7
YernarT <toktaryernar@gmail.com>
2022-03-23 17:23:19 +0600

    feat: :sparkles: Register View Done

--- backend/apps/user/views.py
+++ backend/apps/user/views.py
@@ -1,9 +1,53 @@
-from rest_framework.viewsets import ModelViewSet
+from django.http import JsonResponse
+from django.views.generic import View
+from django.contrib.auth.hashers import check_password, make_password
+
 
 from user.models import User
-from user.serializers import UserSerializer
+from user.data import verify_login, serializer_login
+from user.verify import verify_register
+
+from utils.auth import generate_token, verify_token
+from utils.data import verify_data, get_data, serializer_data
 
 
-class UserView(ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class LoginView(View):
+
+    def post(self, request):
+        data = get_data(request)
+        is_valid, response = verify_login(data)
+        if not is_valid:
+            return response
+
+        # generate_token(user)
+
+        return serializer_login(data)
+
+
+class RegisterView(View):
+
+    def post(self, request):
+        data = get_data(request)
+
+        is_valid, response = verify_register(data)
+        if not is_valid:
+            return response
+
+        # TODO: 校验权限
+        # data.get('role') == 'site admin'
+
+        try:
+            have_same_email_user = User.objects.get(email=data.get('email'))
+        except User.DoesNotExist:
+            have_same_email_user = False
+
+        if have_same_email_user:
+            return JsonResponse({'message': 'Email тіркелген'}, status=400)
+
+        data['password'] = make_password(data['password'])
+        new_user = User.objects.create(**data)
+
+        return JsonResponse({'message': 'тіркелу сәтті болды',
+                             'token': generate_token(new_user.id),
+                             'user': serializer_data(new_user, False)
+                             }, status=201)
