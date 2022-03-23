7961e3f152f3420383a4f767064528003df65254
YernarT <toktaryernar@gmail.com>
2022-03-23 18:15:58 +0600

    update: :hammer: fullname add default value

--- backend/apps/user/models.py
+++ backend/apps/user/models.py
@@ -1,16 +1,21 @@
 from django.db import models
+from datetime import datetime
 
 
 class User(models.Model):
     email = models.EmailField(unique=True, verbose_name='Email')
-    fullname = models.CharField(max_length=50, verbose_name='Аты жөн')
+    fullname = models.CharField(max_length=50,
+                                default='u_' +
+                                datetime.strftime(datetime.now(), '%Y%m%d%H'),
+                                verbose_name='Аты жөн')
     password = models.CharField(max_length=254, verbose_name='Құпия сөз')
     ROLE_CHOICES = (
         ('site admin', 'Сайт әкімшісі'),
         ('dorm manager', 'Жатақхана меңгеруші'),
         ('tenant', 'Жалға алушы'),
     )
-    role = models.CharField(max_length=25, choices=ROLE_CHOICES, verbose_name='рөл')
+    role = models.CharField(
+        max_length=25, choices=ROLE_CHOICES, verbose_name='рөл')
     create_time = models.DateTimeField(
         auto_now_add=True, verbose_name='уақыт')
 
