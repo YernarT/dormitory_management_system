8b4df0c120b2a0ea2ee888e9a2e8843599fd8887
YernarT <toktaryernar@gmail.com>
2022-03-24 23:23:04 +0600

    refactor: :recycle: 使用 css in js 方案重写

---
+++ frontend/src/assets/style/antd-mod-style.js
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const AntdComponentStyleModify = createGlobalStyle`
+	.dms-btn {
+		box-sizing: content-box;
+		padding: 2px 10px;
+	}
+
+	.dms-message {
+		.dms-message-notice-content {
+			.dms-message-custom-content {
+				span[role="img"] {
+					margin-right: 8px;
+				}
+			}
+
+			.dms-message-info {
+				span[role="img"] {
+					color: ${({ theme }) => theme.palette.primary};
+				}
+			}	
+			
+			.dms-message-warning {
+				span[role="img"] {
+					color: ${({ theme }) => theme.palette.warning};
+				}
+			}
+
+			.dms-message-success {
+				span[role="img"] {
+					color: ${({ theme }) => theme.palette.success};
+				}
+			}
+
+			.dms-message-error {
+				span[role="img"] {
+					color: ${({ theme }) => theme.palette.error};
+				}
+			}
+
+			.dms-message-loading {
+				span[role="img"] {
+					color: ${({ theme }) => theme.palette.primary};
+				}
+			}
+		}
+	}
+`;
