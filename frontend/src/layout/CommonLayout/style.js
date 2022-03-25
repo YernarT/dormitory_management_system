d919b6bc982122af9162a2a57e9f8279491177ea
YernarT <toktaryernar@gmail.com>
2022-03-25 14:31:26 +0600

    update: :hammer: add user-action style

--- frontend/src/layout/CommonLayout/style.js
+++ frontend/src/layout/CommonLayout/style.js
@@ -54,6 +54,12 @@ export const CommonLayoutStyledBox = styled.div`
 			.toolbar {
 				width: 100%;
 			}
+
+			.user-action {
+				display: flex;
+				align-items: center;
+				gap: 15px;
+			}
 		}
 
 		.content {
