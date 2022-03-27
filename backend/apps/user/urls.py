f5c86290b519d136c370a929a0094c0ef3b1a4f9
YernarT <toktaryernar@gmail.com>
2022-03-27 17:29:26 +0600

    feat: :sparkles: add edit api url

--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import url
-from user.views import LoginView, RegisterView
+from user.views import LoginView, RegisterView, EditView
+
 
 urlpatterns = [
     url(r'^user/login/$', LoginView.as_view()),
     url(r'^user/register/$', RegisterView.as_view()),
 
-
-    # url(r'^edit/$', EditView.as_view()),
-    # url(r'^edit/avatar/$', EditAvatarView.as_view()),
+    url(r'^user/edit/$', EditView.as_view()),
 ]
 
 app_name = 'user'
