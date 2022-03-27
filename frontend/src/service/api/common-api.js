01408d63d3f27cdabbea8e24f7baeba18b96a4c3
YernarT <toktaryernar@gmail.com>
2022-03-28 00:04:01 +0600

    feat: :sparkles: add change password api

--- frontend/src/service/api/common-api.js
+++ frontend/src/service/api/common-api.js
@@ -2,3 +2,7 @@ import { apiServerInstance } from '../ajax';
 
 // Edit
 export const reqEdit = data => apiServerInstance.post('/user/edit/', data);
+
+// Change password
+export const reqChangePassword = data =>
+	apiServerInstance.post('/user/change_password/', data);
