6c05515c45cfb4937a80403afb6d89f950cbc646
YernarT <toktaryernar@gmail.com>
2022-03-28 18:31:08 +0600

    feat: :sparkles: add feedback api

--- frontend/src/service/api/common-api.js
+++ frontend/src/service/api/common-api.js
@@ -6,3 +6,7 @@ export const reqEdit = data => apiServerInstance.put('/user/edit/', data);
 // Change password
 export const reqChangePassword = data =>
 	apiServerInstance.put('/user/change_password/', data);
+
+// Send feedback
+export const reqSendFeedback = data =>
+	apiServerInstance.post('/user/feedback/', data);
