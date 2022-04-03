d86cbe2365ca8c3568bb0d8e387fe1057a879763
YernarT <toktaryernar@gmail.com>
2022-04-03 19:57:06 +0600

    feat: :sparkles: 404 page done

--- frontend/src/pages/common/PageNotFoundPage/index.jsx
+++ frontend/src/pages/common/PageNotFoundPage/index.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+import { Button, Result, Row } from 'antd';
 
 export default function PageNotFoundPage() {
-	return <div>404</div>;
+	const { push } = useHistory();
+	const { t } = useTranslation();
+
+	return (
+		<Result status="404" title={'404'}>
+			<Row justify="center">
+				<Button type="primary" onClick={() => push('/')}>
+					{t('backToHome')}
+				</Button>
+			</Row>
+		</Result>
+	);
 }
