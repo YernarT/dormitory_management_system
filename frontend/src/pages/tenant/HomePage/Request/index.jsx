54ecc9042a10a518ae86a73ae5b5081333e53136
YernarT <toktaryernar@gmail.com>
2022-04-04 10:17:00 +0600

    update: :hammer: 租客 请求panel

---
+++ frontend/src/pages/tenant/HomePage/Request/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { RequestStyledBox } from './style';
+
+export default function Request() {
+	return (
+		<RequestStyledBox>
+			Жатақханаға орналасу үшін жіберілетін өтінішді өңдеу беті
+		</RequestStyledBox>
+	);
+}
