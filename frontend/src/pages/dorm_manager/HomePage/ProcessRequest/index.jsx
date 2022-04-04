a4903846b5b1ef3bfe37330f6558317e7c4c6dd2
YernarT <toktaryernar@gmail.com>
2022-04-04 10:14:22 +0600

    update: :hammer: 宿管 管理请求panel

---
+++ frontend/src/pages/dorm_manager/HomePage/ProcessRequest/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { ProcessRequestStyledBox } from './style';
+
+export default function ProcessRequest() {
+	return <ProcessRequestStyledBox>ProcessRequest</ProcessRequestStyledBox>;
+}
