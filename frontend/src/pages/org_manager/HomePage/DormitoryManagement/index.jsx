43449af40d997138421df6e131983e558e20b832
YernarT <toktaryernar@gmail.com>
2022-04-09 06:07:32 +0600

    feat: :sparkles: add create dorm fn

--- frontend/src/pages/org_manager/HomePage/DormitoryManagement/index.jsx
+++ frontend/src/pages/org_manager/HomePage/DormitoryManagement/index.jsx
@@ -21,6 +21,7 @@ import {
 	Input,
 	Select,
 	Upload,
+	Space,
 } from 'antd';
 import {
 	DeleteOutlined,
@@ -38,6 +39,7 @@ export default function DormitoryManagement() {
 	const [state, setState] = useSetState({
 		organization: null,
 		dorms: [],
+		dormImages: [],
 		cities: [],
 
 		addDormModalVisibility: false,
@@ -126,8 +128,20 @@ export default function DormitoryManagement() {
 		});
 
 		reqCreateDorm(data)
-			.then(data => {
-				console.log(data);
+			.then(({ dorm, dorm_images, message }) => {
+				antdMessage.success(message);
+				setState(prevState => ({
+					dorms: [...prevState.dorms, dorm],
+					dormImages: [...prevState.dormImages, ...dorm_images],
+					addDormFormData: {
+						name: '',
+						description: '',
+						city: '',
+						address: '',
+						images: [],
+					},
+					addDormModalVisibility: false,
+				}));
 			})
 			.catch(({ message, needExecuteLogout, initialUser }) => {
 				antdMessage.error(message);
@@ -176,89 +190,95 @@ export default function DormitoryManagement() {
 				onOk={handleAddDorm}
 				okText="Құру"
 				onCancel={() => setState({ addDormModalVisibility: false })}>
-				<Input
-					type="text"
-					placeholder="Жатақхана атуы"
-					maxLength={40}
-					value={state.addDormFormData.name}
-					onChange={({ target: { value } }) =>
-						setState(prevState => ({
-							addDormFormData: { ...prevState.addDormFormData, name: value },
-						}))
-					}
-				/>
-
-				<TextArea
-					placeholder={'Сипаттама'}
-					maxLength={254}
-					showCount
-					value={state.addDormFormData.description}
-					onChange={({ target: { value } }) =>
-						setState(prevState => ({
-							addDormFormData: {
-								...prevState.addDormFormData,
-								description: value,
-							},
-						}))
-					}
-				/>
-
-				<Select
-					placeholder="Орналасқан қала"
-					onChange={value =>
-						setState(prevState => ({
-							addDormFormData: { ...prevState.addDormFormData, city: value },
-						}))
-					}>
-					{state.cities.map(city => (
-						<Option key={city.id} value={city.id}>
-							{city.name}
-						</Option>
-					))}
-				</Select>
-
-				<Input
-					type="text"
-					placeholder="Нақты мекен-жай"
-					maxLength={60}
-					value={state.addDormFormData.address}
-					onChange={({ target: { value } }) =>
-						setState(prevState => ({
-							addDormFormData: { ...prevState.addDormFormData, address: value },
-						}))
-					}
-				/>
-
-				<Upload
-					accept="image/*"
-					multiple={true}
-					fileList={state.addDormFormData.images}
-					beforeUpload={file => {
-						if (state.addDormFormData.images.length === 5) {
-							antdMessage.warning('Ең көбінде 5 сурет жариялауға болады');
-							return;
+				<Space direction="vertical" style={{ width: '100%' }} size={15}>
+					<Input
+						type="text"
+						placeholder="Жатақхана атуы"
+						maxLength={40}
+						value={state.addDormFormData.name}
+						onChange={({ target: { value } }) =>
+							setState(prevState => ({
+								addDormFormData: { ...prevState.addDormFormData, name: value },
+							}))
 						}
+					/>
 
-						setState(preState => ({
-							addDormFormData: {
-								...preState.addDormFormData,
-								images: [...preState.addDormFormData.images, file],
-							},
-						}));
-						return false;
-					}}
-					onRemove={file => {
-						setState(preState => ({
-							addDormFormData: {
-								...preState.addDormFormData,
-								images: preState.addDormFormData.images.filter(
-									image => image !== file,
-								),
-							},
-						}));
-					}}>
-					<Button icon={<UploadOutlined />}>Жатақхана суреті</Button>
-				</Upload>
+					<TextArea
+						placeholder={'Сипаттама'}
+						maxLength={254}
+						showCount
+						value={state.addDormFormData.description}
+						onChange={({ target: { value } }) =>
+							setState(prevState => ({
+								addDormFormData: {
+									...prevState.addDormFormData,
+									description: value,
+								},
+							}))
+						}
+					/>
+
+					<Select
+						placeholder="Орналасқан қала"
+						style={{ width: '100%' }}
+						onChange={value =>
+							setState(prevState => ({
+								addDormFormData: { ...prevState.addDormFormData, city: value },
+							}))
+						}>
+						{state.cities.map(city => (
+							<Option key={city.id} value={city.id}>
+								{city.name}
+							</Option>
+						))}
+					</Select>
+
+					<Input
+						type="text"
+						placeholder="Нақты мекен-жай"
+						maxLength={60}
+						value={state.addDormFormData.address}
+						onChange={({ target: { value } }) =>
+							setState(prevState => ({
+								addDormFormData: {
+									...prevState.addDormFormData,
+									address: value,
+								},
+							}))
+						}
+					/>
+
+					<Upload
+						accept="image/*"
+						multiple={true}
+						fileList={state.addDormFormData.images}
+						beforeUpload={file => {
+							if (state.addDormFormData.images.length === 5) {
+								antdMessage.warning('Ең көбінде 5 сурет жариялауға болады');
+								return;
+							}
+
+							setState(preState => ({
+								addDormFormData: {
+									...preState.addDormFormData,
+									images: [...preState.addDormFormData.images, file],
+								},
+							}));
+							return false;
+						}}
+						onRemove={file => {
+							setState(preState => ({
+								addDormFormData: {
+									...preState.addDormFormData,
+									images: preState.addDormFormData.images.filter(
+										image => image !== file,
+									),
+								},
+							}));
+						}}>
+						<Button icon={<UploadOutlined />}>Жатақхана суреті</Button>
+					</Upload>
+				</Space>
 			</Modal>
 		</>
 	);
