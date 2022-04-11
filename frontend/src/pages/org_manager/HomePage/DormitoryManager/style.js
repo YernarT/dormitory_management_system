d83bc74f07768b81263b45107bbc0f3a077fa91e
YernarT <toktaryernar@gmail.com>
2022-04-11 06:41:25 +0600

    feat: :sparkles: get, create dorm_manager

--- frontend/src/pages/org_manager/HomePage/DormitoryManager/style.js
+++ frontend/src/pages/org_manager/HomePage/DormitoryManager/style.js
@@ -1,3 +1,24 @@
 import styled from 'styled-components';
 
-export const DormitoryManagerStyledBox = styled.div``;
+export const DormitoryManagerStyledBox = styled.div`
+	.head {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		margin-bottom: 15px;
+	}
+
+	.dorm-manager {
+		position: relative;
+
+		.delete-btn {
+			position: absolute;
+			top: 6px;
+			right: 6px;
+
+			cursor: pointer;
+			color: ${({ theme }) => theme.palette.error};
+		}
+	}
+`;
