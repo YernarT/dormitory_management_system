a19a56533d32f667dc6f83659f1ee5582896e803
YernarT <toktaryernar@gmail.com>
2022-04-18 23:23:28 +0600

    update: :hammer: 获取所有房间by 宿舍ID

--- frontend/src/service/api/tenant-api.js
+++ frontend/src/service/api/tenant-api.js
@@ -3,5 +3,10 @@ import { apiServerInstance } from '../ajax';
 // Get Dorm (居住的)
 // export const reqGetMyDorm = () => apiServerInstance.get('/dorm/?get_mode=self');
 
-// Get AL  Dorms
-export const reqGetAllDorms = () => apiServerInstance.get('/dorm/');
+// Get ALL  Dorms
+export const reqGetAllDorms = () =>
+	apiServerInstance.get('/dorm/?get_mode=all');
+
+// Get ALL Rooms
+export const reqGetAllRooms = dormId =>
+	apiServerInstance.get(`/dorm/room/?get_mode=by_dorm&dorm_id=${dormId}`);
