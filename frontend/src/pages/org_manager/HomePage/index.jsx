67d6a8b49663c497abe90e4d02fb3a18c31260a3
YernarT <toktaryernar@gmail.com>
2022-04-18 10:35:15 +0600

    update: :hammer: tab 显示控制

--- frontend/src/pages/org_manager/HomePage/index.jsx
+++ frontend/src/pages/org_manager/HomePage/index.jsx
@@ -3,20 +3,20 @@ import { useTranslation } from 'react-i18next';
 import { useRecoilValue, useSetRecoilState, useRecoilState } from 'recoil';
 import { pageAtom, userAtom, dormAtom } from '@/store';
 
+import { useCreation, useResponsive, useRequest, useMount } from 'ahooks';
 import {
-	useCreation,
-	useResponsive,
-	useSetState,
-	useRequest,
-	useMount,
-} from 'ahooks';
-import { reqGetMyOrgaization } from '@/service/api/org-manager-api';
+	reqGetMyOrgaization,
+	reqGetCities,
+	reqGetDorms,
+	reqGetRooms,
+} from '@/service/api/org-manager-api';
 
 import { Tabs, message as antdMessage } from 'antd';
 import { CommonLayout } from '@/layout';
 import Organization from './Organization';
 import DormitoryManagement from './DormitoryManagement';
 import RoomManagement from './RoomManagement';
+import BedManagement from './BedManagement';
 import ProcessRequest from './ProcessRequest';
 import DormitoryManager from './DormitoryManager';
 
@@ -31,18 +31,41 @@ export default function HomePage() {
 	const page = useRecoilValue(pageAtom);
 	const [dorm, setDorm] = useRecoilState(dormAtom);
 
+	// 获取机构
 	const { runAsync: runReqGetMyOrganization } = useRequest(
 		() => reqGetMyOrgaization(),
 		{
 			manual: true,
 		},
 	);
+	// 获取城市
+	const { runAsync: runReqGetCities } = useRequest(() => reqGetCities(), {
+		manual: true,
+	});
+	// 获取宿舍
+	const { runAsync: runReqGetDorms } = useRequest(() => reqGetDorms(), {
+		manual: true,
+	});
+	// 获取房间
+	const { runAsync: runReqGetRooms } = useRequest(() => reqGetRooms(), {
+		manual: true,
+	});
 
 	useMount(() => {
-		runReqGetMyOrganization()
-			.then(({ organization }) =>
-				setDorm(prevState => ({ ...prevState, organization })),
-			)
+		Promise.all([
+			runReqGetMyOrganization(),
+			runReqGetCities(),
+			runReqGetDorms(),
+			runReqGetRooms(),
+		])
+			.then(([{ organization }, { cities }, { dorms }, { rooms }]) => {
+				setDorm({
+					organization,
+					hasCity: Boolean(cities.length),
+					hasDorm: Boolean(dorms.length),
+					hasRoom: Boolean(rooms.length),
+				});
+			})
 			.catch(({ message, needExecuteLogout, initialUser }) => {
 				antdMessage.error(message);
 
@@ -69,21 +92,28 @@ export default function HomePage() {
 				key: 'room_management',
 				tabName: t('org_manager_tabName_room_management'),
 				tabContent: <RoomManagement />,
+				disabled: !dorm.hasDorm,
+			},
+			{
+				key: 'bed_management',
+				tabName: t('org_manager_tabName_bed_management'),
+				tabContent: <BedManagement />,
+				disabled: !dorm.hasRoom,
 			},
 			{
 				key: 'process_request',
 				tabName: t('org_manager_tabName_process_request'),
 				tabContent: <ProcessRequest />,
-				disabled: Boolean(!dorm.organization),
+				disabled: !dorm.hasRoom,
 			},
-			{
-				key: 'dormitory_manager',
-				tabName: t('org_manager_tabName_dormitory_manager'),
-				tabContent: <DormitoryManager />,
-				disabled: Boolean(!dorm.organization),
-			},
+			// {
+			// 	key: 'dormitory_manager',
+			// 	tabName: t('org_manager_tabName_dormitory_manager'),
+			// 	tabContent: <DormitoryManager />,
+			// 	disabled: !dorm.hasDorm,
+			// },
 		],
-		[page.locale, dorm.organization],
+		[page.locale, dorm],
 	);
 
 	return (
