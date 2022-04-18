e395be06c8fef38aad9c77cdcc68eb9691b70254
YernarT <toktaryernar@gmail.com>
2022-04-18 23:22:35 +0600

    update: :hammer: 租客点击宿舍跳转到房间列表

--- frontend/src/pages/tenant/HomePage/Dormitory/index.jsx
+++ frontend/src/pages/tenant/HomePage/Dormitory/index.jsx
@@ -1,51 +1,59 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
 import { userAtom } from '@/store';
 import { useTranslation } from 'react-i18next';
 
-import { useRequest, useMount, useSetState } from 'ahooks';
+import { useRequest, useSetState } from 'ahooks';
 import { reqGetAllDorms } from '@/service/api/tenant-api';
 
-import { message as antdMessage, Button, Card, Empty } from 'antd';
+import { message as antdMessage, Empty, Space } from 'antd';
+import { DormCard } from '@/components/dorm';
 import { DormitoryStyledBox } from './style';
 
 export default function Dormitory() {
+	const history = useHistory();
 	const setUser = useSetRecoilState(userAtom);
 	const { t } = useTranslation();
 	const [state, setState] = useSetState({
 		dorms: [],
 	});
 
-	const { runAsync: runReqGetAllDorm, loading: loadingReqGetAllDorm } =
-		useRequest(() => reqGetAllDorms(), {
-			manual: true,
-		});
+	const { loading: loadingReqGetAllDorm } = useRequest(reqGetAllDorms, {
+		onSuccess({ dorms }) {
+			setState({ dorms });
+		},
+		onError({ message, needExecuteLogout, initialUser }) {
+			antdMessage.error(message);
 
-	useMount(() => {
-		runReqGetAllDorm()
-			.then(({ dorms }) => {
-				setState({ dorms });
-			})
-			.catch(({ message, needExecuteLogout, initialUser }) => {
-				antdMessage.error(message);
-
-				if (needExecuteLogout) {
-					setUser(initialUser);
-				}
-			});
+			if (needExecuteLogout) {
+				setUser(initialUser);
+			}
+		},
 	});
 
 	return (
 		<DormitoryStyledBox>
-			{state.dorms.length ? (
-				<ul>
-					{state.dorms.map(dorm => (
-						<li key={dorm.id}>Жатақхана атауы: {dorm.name}</li>
-					))}
-				</ul>
-			) : (
-				<Empty description="Қазірше жатақхана жоқ" />
-			)}
+			<div className="dorms">
+				{state.dorms.length > 0 ? (
+					<Space direction="vertical" size={15}>
+						{state.dorms.map(dorm => (
+							<DormCard
+								key={dorm.id}
+								dorm={dorm}
+								showDeleteBtn={false}
+								loading={loadingReqGetAllDorm}
+								clickable
+								onClick={() => {
+									history.push('/rooms', { dormId: dorm.id });
+								}}
+							/>
+						))}
+					</Space>
+				) : (
+					<Empty description="Жатақхана жоқ" />
+				)}
+			</div>
 		</DormitoryStyledBox>
 	);
 }
