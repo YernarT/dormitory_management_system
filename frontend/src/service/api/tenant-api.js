c8b1b2f7ed7215b8495861654b03d7a59ea2f94f
YernarT <toktaryernar@gmail.com>
2022-04-20 10:17:42 +0600

    feat: :sparkles: get all beds for tenant

--- frontend/src/service/api/tenant-api.js
+++ frontend/src/service/api/tenant-api.js
@@ -10,3 +10,7 @@ export const reqGetAllDorms = () =>
 // Get ALL Rooms
 export const reqGetAllRooms = dormId =>
 	apiServerInstance.get(`/dorm/room/?get_mode=by_dorm&dorm_id=${dormId}`);
+
+// Get ALL Beds
+export const reqGetAllBeds = roomId =>
+	apiServerInstance.get(`/dorm/bed/?get_mode=by_room&room_id=${roomId}`);
