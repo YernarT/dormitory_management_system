2f94c86019a4c25a8d368b8cf88f8ba9efcd7d18
YernarT <toktaryernar@gmail.com>
2022-04-21 20:39:09 +0600

    update: :hammer: 更新处理请求面板

--- frontend/src/pages/org_manager/HomePage/ProcessRequest/index.jsx
+++ frontend/src/pages/org_manager/HomePage/ProcessRequest/index.jsx
@@ -3,22 +3,25 @@ import { useSetRecoilState } from 'recoil';
 import { userAtom } from '@/store';
 
 import { useRequest, useSetState } from 'ahooks';
-import { reqGetRequests } from '@/service/api/org-manager-api';
+import { reqGetOrders } from '@/service/api/org-manager-api';
 
 import { message as antdMessage, Space, Empty } from 'antd';
-import { RequestCard } from '@/components/dorm';
+import { OrderCard } from '@/components/dorm';
 import { ProcessRequestStyledBox } from './style';
 
 export default function ProcessRequest() {
 	const setUser = useSetRecoilState(userAtom);
 	const [state, setState] = useSetState({
-		requests: [],
+		orders: [],
 	});
 
 	// 获取所有请求
-	const { loading: loadingReqGetRequests } = useRequest(reqGetRequests, {
-		onSuccess({ requests }) {
-			setState({ requests });
+	useRequest(reqGetOrders, {
+		pollingInterval: 1500,
+		pollingWhenHidden: false,
+
+		onSuccess({ orders }) {
+			setState({ orders });
 		},
 		onError({ message, needExecuteLogout, initialUser }) {
 			antdMessage.error(message);
@@ -29,10 +32,12 @@ export default function ProcessRequest() {
 		},
 	});
 
-	// 处理 删除请求
-	const handleDeleteRequest = id => {
-		console.log(id);
+	// 处理 请求状态的更新
+	const afterHandleResult = orders => {
+		setState({ orders });
 	};
+
+	const notAgreeOrders = state.orders.filter(order => !order.status);
 
 	return (
 		<ProcessRequestStyledBox>
@@ -40,16 +45,15 @@ export default function ProcessRequest() {
 				<h2 className="title">Өтініштер</h2>
 			</div>
 
-			<div className="requests">
-				{state.requests.length > 0 ? (
+			<div className="orders">
+				{notAgreeOrders.length > 0 ? (
 					<Space direction="vertical" size={15}>
-						{state.requests.map(request => (
-							<RequestCard
-								key={request.id}
-								request={request}
-								loading={loadingReqGetRequests}
+						{notAgreeOrders.map(order => (
+							<OrderCard
+								key={order.id}
+								order={order}
 								showDeleteBtn={true}
-								handleDelete={handleDeleteRequest}
+								afterHandleResult={afterHandleResult}
 							/>
 						))}
 					</Space>
