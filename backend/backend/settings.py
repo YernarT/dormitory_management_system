36a27e6a34395a612bef86d061bb7a16da3e7727
YernarT <toktaryernar@gmail.com>
2022-04-26 20:11:04 +0600

    update: :hammer: use mysql db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,11 +74,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dms',
+        'USER': 'root',
+        'PASSWORD': 'password-0',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 # Templates
 TEMPLATES = [
