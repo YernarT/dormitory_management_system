40efaeff2bdae2471be4d75015d03fbf58cff795
YernarT <toktaryernar@gmail.com>
2022-07-22 00:10:33 +0600

    fix: :bug: role 属性不带空格

--- frontend/src/utils/role.ts
+++ frontend/src/utils/role.ts
@@ -1,23 +1,23 @@
 // 角色
-export type allowedRole = 'GUEST' | 'TENANT' | 'DORM_MANAGER' | 'ADMIN';
+export type allowedRole = 'GUEST' | 'TENANT' | 'DORM MANAGER' | 'ADMIN';
 
 // 角色对象
 export const role: {
 	GUEST: 'GUEST';
 	TENANT: 'TENANT';
-	DORM_MANAGER: 'DORM_MANAGER';
+	DORM_MANAGER: 'DORM MANAGER';
 	ADMIN: 'ADMIN';
 
 	// 所有角色
-	all: () => ['GUEST', 'TENANT', 'DORM_MANAGER', 'ADMIN'];
+	all: () => ['GUEST', 'TENANT', 'DORM MANAGER', 'ADMIN'];
 } = {
 	GUEST: 'GUEST',
 	TENANT: 'TENANT',
-	DORM_MANAGER: 'DORM_MANAGER',
+	DORM_MANAGER: 'DORM MANAGER',
 	ADMIN: 'ADMIN',
 
 	// 所有角色
 	all() {
-		return ['GUEST', 'TENANT', 'DORM_MANAGER', 'ADMIN'];
+		return ['GUEST', 'TENANT', 'DORM MANAGER', 'ADMIN'];
 	},
 };
