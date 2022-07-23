f6457df3f0d1009b327c731dfba5612bc7aba3eb
YernarT <toktaryernar@gmail.com>
2022-07-23 12:45:57 +0600

    rename: :pencil2: 图片改名

--- frontend/src/assets/image/index.ts
+++ frontend/src/assets/image/index.ts
@@ -1,4 +1,4 @@
 // @ts-ignore 暂无解决方法
 export { default as favicon } from './favicon.svg';
 // @ts-ignore 暂无解决方法
-export { default as welcomeBG } from './welcome-background.jpg';
+export { default as universityPhoto } from './university-photo.jpg';
