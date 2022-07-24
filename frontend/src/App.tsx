7d0ebc78ef73bc9c3226201837ae2020fc33ee25
YernarT <toktaryernar@gmail.com>
2022-07-24 18:25:00 +0600

    refactor: :recycle: 使用 ts 重构

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+// React & 周边库
 import { BrowserRouter } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { userAtom, pageAtom } from '@/store';
 
+// 业务库
+import { useEventListener, useCreation } from 'ahooks';
+import { localStorage, getAntdLocale } from '@/utils';
+
+// 组件
 import {
 	ConfigProvider as AntdConfigProvider,
 	message,
 	notification,
 } from 'antd';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
-
 import { RouteGuard, CssBaseLine } from '@/components/common';
 
-import { useEventListener, useCreation } from 'ahooks';
-import { localStorage, getAntdLocale } from '@/utils';
-
-import { useRecoilValue } from 'recoil';
-import { userAtom, pageAtom, dormAtom } from '@/store';
-
-import getTheme from '@/assets/theme';
-import { AntdComponentStyleModify } from '@/assets/style/antd-mod-style';
+// 其他资源
+import { theme } from '@/assets/theme';
 import routes from '@/routes';
 
 // global config for antd message
@@ -34,17 +34,13 @@ notification.config({
 export default function App() {
 	const user = useRecoilValue(userAtom);
 	const page = useRecoilValue(pageAtom);
-	const dorm = useRecoilValue(dormAtom);
 
 	// Refresh the page to save the data in Recoil to LocalStorage
 	useEventListener('beforeunload', () => {
 		localStorage.set('user', user);
 		localStorage.set('page', page);
-		localStorage.set('dorm', dorm);
 	});
 
-	// Theme object
-	const theme = useCreation(() => getTheme(), []);
 	// Locale object
 	const locale = useCreation(() => getAntdLocale(page.locale), [page.locale]);
 
@@ -58,8 +54,6 @@ export default function App() {
 				<BrowserRouter>
 					{/* 全局样式 */}
 					<CssBaseLine />
-					{/* Antd design 组件样式重写 */}
-					<AntdComponentStyleModify />
 					{/* 路由守卫 */}
 					<RouteGuard routes={routes} />
 				</BrowserRouter>
