09680db2c26272a99de039177d7ce1d44e73f318
YernarT <toktaryernar@gmail.com>
2022-07-24 18:15:19 +0600

    wip: :construction: 宿舍列表页

---
+++ frontend/src/pages/guest/DormListPage/index.tsx
@@ -0,0 +1,12 @@
+// 组件
+import { GuestLayout } from '@/components/layout';
+// 样式组件
+import { DormListPageStyled } from './style';
+
+export default function DormListPage() {
+	return (
+		<GuestLayout>
+			<DormListPageStyled>DormListPage</DormListPageStyled>
+		</GuestLayout>
+	);
+}
