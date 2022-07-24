5c285fe3a4c9c261e9d14dd068015e4b801e46ac
YernarT <toktaryernar@gmail.com>
2022-07-24 18:09:02 +0600

    update: :hammer: content 区域使用 container(width) 样式

--- frontend/src/components/layout/GuestLayout/style.ts
+++ frontend/src/components/layout/GuestLayout/style.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
-import { layoutStyle } from '@/assets/style';
+import { layoutStyle, container } from '@/assets/style';
 
 export const GuestLayoutStyled = styled.div`
 	${layoutStyle}
 
 	.content {
 		/* 减掉 Header 高度 */
+		${container('width')}
 		height: calc(100% - 64px);
 
 		position: relative;
