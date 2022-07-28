0512278156ba847f16ecec4a1e29de0e44598e2d
YernarT <toktaryernar@gmail.com>
2022-07-28 15:07:18 +0600

    feat: :sparkles: 修补 alias 工具函数

--- backend/moduleAlias.js
+++ backend/moduleAlias.js
@@ -4,21 +4,49 @@
 
 const fs = require('fs');
 
-const BASE_URL = './dist';
+/**
+ * 获取指定目录下 所有文件路径
+ *
+ * @param { string } entry 入口, 根目录路径
+ * @param { string[]? } fileList 文件路径列表
+ *
+ * @return { string[] } 所有文件路径列表
+ */
+function walk(entry, fileList = []) {
+	const files = fs.readdirSync(entry);
 
-fs.readdirSync(BASE_URL).forEach(path => {
-	let fullPath = `${BASE_URL}/${path}`;
+	files.forEach(file => {
+		let fullPath = `${entry}/${file}`;
+		let stats = fs.statSync(fullPath);
 
-	fs.stat(fullPath, (err, stats) => {
-		if (err) {
-			console.log('报错了: ', err);
-			throw err;
+		if (stats.isDirectory()) {
+			walk(fullPath, fileList);
 		}
 
 		if (stats.isFile()) {
-			let content = fs.readFileSync(fullPath).toString();
-			content = content.replaceAll('require("@', 'require("AAA');
-			console.log(content);
+			fileList.push(fullPath);
 		}
 	});
+
+	return fileList;
+}
+
+// 入口, 根目录路径
+const BASE_URL = 'dist';
+// 所有文件路径列表
+const fileList = walk(BASE_URL);
+
+fileList.forEach(file => {
+	let structure = file.split('/');
+	let content = fs.readFileSync(file).toString();
+
+	if (content.includes('require("@')) {
+		fs.writeFileSync(
+			file,
+			content.replaceAll(
+				'require("@',
+				`require("${'.'.repeat(structure.length - 1)}`,
+			),
+		);
+	}
 });
