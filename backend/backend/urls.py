71d26ae19efa557de7c4768883e7d8ab2842c807
YernarT <toktaryernar@gmail.com>
2023-04-01 23:33:17 +0600

    init: :rainbow: django backend

---
+++ backend/backend/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+from django.views.static import serve
+
+from backend import settings
+
+urlpatterns = [
+    url('^admin/', admin.site.urls),
+
+    # url('^api/', include('user.urls')),
+    # url('^api/', include('dorm.urls')),
+    # url('^api/', include('order.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,
+        {"document_root": settings.MEDIA_ROOT}),
+]
