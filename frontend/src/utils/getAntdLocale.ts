3b60f5289476f606f9d61f6973688d384f8fa28f
YernarT <toktaryernar@gmail.com>
2023-04-05 12:56:42 +0600

    update: :hammer: remove moment

--- frontend/src/utils/getAntdLocale.ts
+++ frontend/src/utils/getAntdLocale.ts
@@ -3,19 +3,13 @@ import type { allowedLocale } from '@/i18n';
 import kkKZ from 'antd/lib/locale/kk_KZ';
 import enUS from 'antd/lib/locale/en_US';
 
-import moment from 'moment';
-import 'moment/locale/kk';
-import 'moment/locale/en-nz';
-
 export default function getAntdLocale(locale: allowedLocale) {
 	switch (locale) {
 		case 'enUS':
-			moment.locale('en-nz');
 			return enUS;
 
 		case 'kkKZ':
 		default:
-			moment.locale('kk');
 			return kkKZ;
 	}
 }
